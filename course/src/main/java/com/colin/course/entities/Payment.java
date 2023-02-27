18926c037305a114a082156dfb4d4a7e47173449
RafaColin <faelnek@gmail.com>
2023-02-27 09:58:56 -0300

    Subtotal & Total methods

--- course/src/main/java/com/colin/course/entities/Payment.java
+++ course/src/main/java/com/colin/course/entities/Payment.java
@@ -4,10 +4,13 @@ import java.io.Serializable;
 import java.time.Instant;
 import java.util.Objects;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.MapsId;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 
@@ -23,7 +26,10 @@ public class Payment implements Serializable{
 	private Long id;
 	private Instant moment;
 	
+	
+	@JsonIgnore
 	@OneToOne
+	@MapsId
 	private Order order;
 	
 	public Payment() {
