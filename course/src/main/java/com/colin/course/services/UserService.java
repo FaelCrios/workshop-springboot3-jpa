9c414331f6eb48ea43d93119bb5bc2f34e8c40e2
RafaColin <faelnek@gmail.com>
2023-02-27 13:01:08 -0300

    Exception handling update

--- course/src/main/java/com/colin/course/services/UserService.java
+++ course/src/main/java/com/colin/course/services/UserService.java
@@ -4,11 +4,16 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.colin.course.entities.User;
 import com.colin.course.repositories.UserRepository;
+import com.colin.course.resources.exceptions.DatabaseException;
 import com.colin.course.services.exceptions.ResourceNotFoundException;
+
+import jakarta.persistence.EntityNotFoundException;
 
 @Service
 public class UserService {
@@ -30,14 +35,23 @@ public class UserService {
 	}
 	
 	public void delete(Long id) {
+		try {
 		repository.deleteById(id);
+	} catch(EmptyResultDataAccessException e) {
+		throw new ResourceNotFoundException(id);
+	} catch(DataIntegrityViolationException e) {
+		throw new DatabaseException(e.getMessage());
+	}
 	}
 	
 	public User update(Long id, User obj) {
+	try {
 		User entity = repository.getReferenceById(id);
 		updateData(entity, obj);
 		return repository.save(entity);
-	}
+	}catch(EntityNotFoundException e) {
+		throw new ResourceNotFoundException(id);
+	}}
 
 	private void updateData(User entity, User obj) {
 		entity.setName(obj.getName());
